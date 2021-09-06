79c3d44dd7fd80e5cee79310cdaedf740c87e8a2
loksblnine <illya01@ukr.net>
2021-09-06 18:49:11 +0300

    feat: setUser

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -12,25 +12,22 @@ export const registration = async (email, password) => {
 }
 
 export const login = async (email, password) => {
-    const resp = await fetch(constants.SERVER_URL + `/login`, {
+    return await fetch(constants.SERVER_URL + `/login`, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({email, password})
     })
         .then(response => response.json())
-        .then(data => console.log(data.token))
-    localStorage.setItem('token', resp.token)
-    console.log(jwt_decode(resp.token))
-    return true
+        .then(data => localStorage.setItem('token', data.token))
 }
 
 
 export const check = async () => {
-    const {data} = await fetch(constants.SERVER_URL + `/login`, {
+    return await fetch(constants.SERVER_URL + `/login`, {
         method: "GET",
         headers: {"Content-Type": "application/json", "Authorization": `Bearer ${localStorage.getItem('token')}`}
     })
         .then(response => response.json())
         .then(data => localStorage.setItem('token', data.token))
-    return jwt_decode(data.token)
+    // return jwt_decode(data.token)
 }
