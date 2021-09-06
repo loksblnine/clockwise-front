d1a54b28d1e442512811db34e8448a3b4869ec77
loksblnine <illya01@ukr.net>
2021-09-06 15:27:08 +0300

    add useContext

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import UserStore from "./store/UserStore";
 
+export const Context = createContext(null)
+console.log(process.env.NODE_ENV)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Context.Provider value={{
+        user: new UserStore()
+    }}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Context.Provider>,
+    document.getElementById('root')
 );
