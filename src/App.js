1b4888e1f2330b374e3dde17d502e8d3d987c163
loksblnine <illya01@ukr.net>
2021-09-06 15:26:47 +0300

    remove trash

--- src/App.js
+++ src/App.js
@@ -8,21 +8,14 @@ import * as constants from "./constants"
 import './App.css';
 
 //components
-import AdminPanel from "./components/content/admin-content/login-form/AdminPanel";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import OrderForm from "./components/content/customer-content/orderform/OrderForm";
-import MasterView from "./components/content/customer-content/mastersview/MasterView";
-import LoginForm from "./components/content/admin-content/login-form/LoginForm";
-import ListCities from "./components/content/admin-content/cities/ListCities";
-import ListCustomers from "./components/content/admin-content/customers/ListCustomers";
-import ListMasters from "./components/content/admin-content/masters/ListMasters";
-import ListOrders from "./components/content/admin-content/orders/ListOrders";
 import {Context} from "./index";
 import AppRouter from "./components/AppRouter";
+import {observer} from "mobx-react-lite";
 
 
-const App = () => {
+const App = observer(() => {
+    const {user} = useContext(Context)
     return (
         <Fragment>
             <Router>
@@ -33,5 +26,5 @@ const App = () => {
 
         </Fragment>
     );
-}
+})
 export default App;
