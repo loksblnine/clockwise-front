2c471fe61f8ca640f853b5759929a9fff9ce7d4a
loksblnine <illya01@ukr.net>
2021-09-06 15:24:36 +0300

    update logic

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
 import OrderForm from "./components/content/customer-content/orderform/OrderForm";
 import MasterView from "./components/content/customer-content/mastersview/MasterView";
-import LoginForm from "./components/content/admin-content/login-form/LoginForm";
-import AdminPanel from "./components/content/admin-content/login-form/AdminPanel";
 import ListCities from "./components/content/admin-content/cities/ListCities";
 import ListOrders from "./components/content/admin-content/orders/ListOrders";
 import ListMasters from "./components/content/admin-content/masters/ListMasters";
 import ListCustomers from "./components/content/admin-content/customers/ListCustomers";
+import LoginForm from "./components/content/customer-content/login-form/LoginForm";
+import AdminPanel from "./components/content/admin-content/admin-panel/AdminPanel";
 
-export const SERVER_URL = process.env.NODE_ENV === "production"
-    ? "https://enigmatic-spire-58695.herokuapp.com": "http://localhost:5000"
+export const SERVER_URL = process.env.NODE_ENV === "development"
+    ? "http://localhost:5000" : "https://enigmatic-spire-58695.herokuapp.com"
 
 export const WORK_TYPES = {
     1: {
@@ -51,28 +51,35 @@ export const customerRoutes = [
     {
         path: '/',
         Component: OrderForm
-    }, {
+    },
+    {
         path: '/masters_choosing',
         Component: MasterView
-    }, {
+    },
+    {
         path: '/login',
         Component: LoginForm
     }
 ]
+
 export const authRoutes = [
     {
         path: '/access_succeed',
         Component: AdminPanel
-    }, {
+    },
+    {
         path: '/cities',
         Component: ListCities
-    }, {
+    },
+    {
         path: '/orders',
         Component: ListOrders
-    }, {
+    },
+    {
         path: '/masters',
         Component: ListMasters
-    }, {
+    },
+    {
         path: '/customers',
         Component: ListCustomers
     }
