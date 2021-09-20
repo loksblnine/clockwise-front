bdbe46f6acf5541da37ac094b85b16669ee85130
loksblnine <illya01@ukr.net>
2021-09-20 11:19:27 +0300

    add 404 page

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,17 +3,19 @@ import {Route, Switch} from "react-router-dom";
 import {Context} from "../index";
 import {authRoutes, customerRoutes} from "../constants";
 import {observer} from "mobx-react-lite";
+import NotFound from "../http/NotFound";
 
 const AppRouter = observer((props) => {
     const {user} = useContext(Context)
     return (
         <Switch>
             {user.isAuth && authRoutes.map(({path, Component}) =>
-                <Route path={path} component={Component} exact/>
+                <Route path={path} component={Component} key={path} exact/>
             )}
             {customerRoutes.map(({path, Component}) =>
-                <Route path={path} component={Component} exact/>
+                <Route path={path} component={Component} key={path} exact/>
             )}
+            <Route path="*" component={NotFound} />
         </Switch>
     );
 })
