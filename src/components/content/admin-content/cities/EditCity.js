85253a97ffff979e94fc0f40ce434fb40d93a24f
loksblnine <illya01@ukr.net>
2021-09-20 12:58:58 +0300

    update: validation modals

--- src/components/content/admin-content/cities/EditCity.js
+++ src/components/content/admin-content/cities/EditCity.js
@@ -5,6 +5,7 @@ import {useHistory} from "react-router-dom";
 const EditCity = ({city}) => {
     const [city_name, setCityName] = useState(city.city_name)
     const history = useHistory()
+
     const updateCity = async (e) => {
         e.preventDefault()
         try {
@@ -23,7 +24,6 @@ const EditCity = ({city}) => {
     }
     return (
         <Fragment>
-            <form>
                 <button type="button" className="btn btn-warning" data-toggle="modal"
                         data-target={`#id${city.city_id}`}>
                     Редактировать
@@ -31,32 +31,39 @@ const EditCity = ({city}) => {
 
                 <div className="modal fade" id={`id${city.city_id}`} tabIndex="-1" role="dialog"
                      aria-labelledby="exampleModalLabel" aria-hidden="true">
-
                     <div className="modal-dialog" role="document">
                         <div className="modal-content">
-                            <div className="modal-header">
-                                <h2 className="modal-title" id="exampleModalLabel">Редактировать город</h2>
-                                <button type="button" className="btn-close" data-dismiss="modal"
-                                        aria-label="Close"/>
-                            </div>
-                            <div className="modal-body">
-                                <label htmlFor={`city_name`}>Название города</label>
-                                <input className="form-control" placeholder="Город" value={city_name} name={`city_name`}
-                                       required onChange={e => setCityName(e.target.value)}/>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Закрыть
-                                </button>
-                                <button type="submit" className="btn btn-primary"
-                                        onClick={e => updateCity(e)}>Сохранить изменения
-                                </button>
-                            </div>
+                            <form onSubmit={event => updateCity(event)}>
+                                <div className="modal-header">
+                                    <h2 className="modal-title" id="exampleModalLabel">Редактировать город</h2>
+                                    <button type="button" className="btn-close" data-dismiss="modal"
+                                            aria-label="Close"/>
+                                </div>
+
+                                <div className="modal-body">
+                                    <label htmlFor={`city_name`}>Название города</label>
+                                    <input className="form-control" placeholder="Город" value={city_name}
+                                           name={`city_name`}
+                                           required onChange={e => setCityName(e.target.value)}/>
+                                </div>
+
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Закрыть
+                                    </button>
+                                    <button type="submit" className="btn btn-primary"
+                                    >Сохранить изменения
+                                    </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
-            </form>
         </Fragment>
     )
 }
+//modal content w form
+//required
+//button type submit
+//
 
 export default EditCity;
--- src/components/content/admin-content/cities/InputCity.js
+++ src/components/content/admin-content/cities/InputCity.js
@@ -34,24 +34,26 @@ const InputCity = () => {
                  aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="modal-title" id="exampleModalLabel">Добавить город</h1>
-                            <button type="button" className="close" data-dismiss="modal">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <label htmlFor={`name`}>Название города</label>
-                            <input className="form-control" placeholder="Город" value={city_name} name={`name`}
-                                   onChange={e => setCityName(e.target.value)} required/>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Закрыть
-                            </button>
-                            <button type="submit" className="btn btn-primary"
-                                    onClick={e => onSubmitForm(e)}>Сохранить изменения
-                            </button>
-                        </div>
+                        <form onSubmit={event => onSubmitForm(event)}>
+                            <div className="modal-header">
+                                <h1 className="modal-title" id="exampleModalLabel">Добавить город</h1>
+                                <button type="button" className="close" data-dismiss="modal">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <label htmlFor={`name`}>Название города</label>
+                                <input className="form-control" placeholder="Город" value={city_name} name={`name`}
+                                       onChange={e => setCityName(e.target.value)} required/>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Закрыть
+                                </button>
+                                <button type="submit" className="btn btn-primary">
+                                    Сохранить изменения
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
