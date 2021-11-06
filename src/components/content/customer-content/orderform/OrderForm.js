eab388d4515ce3a1ed1f70f4fe56c43d9fee87ca
loksblnine <illya01@ukr.net>
2021-11-06 08:37:42 +0200

    fix code style

--- src/components/content/customer-content/orderform/OrderForm.js
+++ src/components/content/customer-content/orderform/OrderForm.js
@@ -64,79 +64,75 @@ const OrderForm = observer((props) => {
                 })
             },
         });
-
         return (
             <div style={orderPageStyle}>
                 <form onSubmit={formik.handleSubmit} className={`form`}>
                     <div className="form-group">
-                        <label className={`text`} htmlFor={`name`}>Имя</label>
-                        <input type={`name`} id={`name`} name={`name`} key={`name`}
+                        <label className="text" htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" key="email"
+                               value={formik.values.email} onChange={formik.handleChange}
+                               className="form-control" placeholder="email"
+                               required/>
+                        {formik.errors.email ? <div className={`error`}>{formik.errors.email}</div> : null}
+                    </div>
+                    <div className="form-group">
+                        <label className="text" htmlFor="name">Имя</label>
+                        <input type="name" id="name" name="name" key="name"
                                value={formik.values.name} onChange={formik.handleChange}
-                               className={"form-control"} placeholder="Имя"
+                               className="form-control" placeholder="Имя"
                                required/>
                         {formik.errors.name ? <div className={`error`}>{formik.errors.name}</div> : null}
                     </div>
                     <div className="form-group">
-                        <label className={`text`} htmlFor="email">Email</label>
-                        <input type={`email`} id={`email`} name={`email`} key={`email`}
-                               value={formik.values.email} onChange={formik.handleChange}
-                               className={"form-control"} placeholder="email"
-                               required/>
-                        {formik.errors.email ? <div className={`error`}>{formik.errors.email}</div> : null}
-                    </div>
-
-                    <div className="form-group">
-                        <label className={`text`} htmlFor={`city`}>Город</label>
-                        <select id={`city`} name={`city`} key={`city`}
+                        <label className="text" htmlFor="city">Город</label>
+                        <select id="city" name="city" key="city"
                                 value={formik.values.city} onChange={formik.handleChange}
-                                className={"form-control"}
+                                className="form-control"
                                 placeholder="Выберите ваш город"
                                 required>
                             {cities?.map(city =>
                                 <option key={city.city_id} value={city.city_id}>{city.city_name} </option>)}
                         </select>
                     </div>
-
                     <div className="form-group">
-                        <label className={`text`} htmlFor={`date`}>Введите дату заказа </label>
-                        <input type={`date`} id={`date`} name={`date`} className={`form-control react-datetime-picker`}
-                               min={constants.DATE_FROM} max={constants.DATE_TO} key={`date`}
+                        <label className="text" htmlFor="date">Введите дату заказа </label>
+                        <input type="date" id="date" name="date" className="form-control react-datetime-picker"
+                               min={constants.DATE_FROM} max={constants.DATE_TO} key="date"
                                required pattern="[0-9]{4}.[0-9]{2}.[0-9]{2}"
                                value={formik.values.date}
                                onChange={formik.handleChange}/>
                     </div>
-
                     <div className="form-group">
-                        <label className={`text`} htmlFor={`time`}>Время заказа (8:00 - 17:00) </label>
-                        <input type={`time`} name={`time`} className={`form-control timepicker`}
-                               min={constants.TIME_FROM} max={constants.TIME_TO} key={`time`}
-                               required step='3600' value={formik.values.time}
+                        <label className="text" htmlFor="time">Время заказа (8:00 - 17:00) </label>
+                        <input type="time" name="time" className="form-control timepicker"
+                               min={constants.TIME_FROM} max={constants.TIME_TO} key="time"
+                               required step="3600" value={formik.values.time}
                                pattern="([01]?[0-9]|2[0-3]):[0][0]" id="24h"
                                onChange={formik.handleChange}/>
                     </div>
                     <div className="form-group">
-                        <label className={`text`} htmlFor={`type`}> Выберите тип поломки </label>
-                        <div role="group" aria-labelledby="my-radio-group" className={"radio-toolbar"}>
+                        <label className="text" htmlFor="type"> Выберите тип поломки </label>
+                        <div role="group" aria-labelledby="my-radio-group" className="radio-toolbar">
                             <label className="miro-radiobutton">
-                                <input type={`radio`} value={`1`} name={`type`}
-                                       id={`radio1`}
-                                       key={`radio1`}
+                                <input type="radio" value="1" name="type"
+                                       id="radio1"
+                                       key="radio1"
                                        checked={formik.values.type === '1'}
                                        onChange={formik.handleChange}/>
                                 <span>Маленькие часы </span>
                             </label>
                             <label className="miro-radiobutton">
-                                <input type={`radio`} value={`2`} name={`type`}
-                                       id={`radio2`}
-                                       key={`radio2`}
+                                <input type="radio" value="2" name="type"
+                                       id="radio2"
+                                       key="radio2"
                                        checked={formik.values.type === '2'}
                                        onChange={formik.handleChange}/>
                                 <span>Средние часы </span>
                             </label>
                             <label className="miro-radiobutton">
-                                <input type={`radio`} value={`3`} name={`type`}
-                                       id={`radio3`}
-                                       key={`radio3`}
+                                <input type="radio" value="3" name="type"
+                                       id="radio3"
+                                       key="radio3"
                                        checked={formik.values.type === '3'}
                                        onChange={formik.handleChange}/>
                                 <span>Большие часы </span>
