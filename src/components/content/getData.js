676b008ed032bced52f9ed200cbd87178826c2ba
loksblnine <illya01@ukr.net>
2021-11-08 14:46:29 +0200

    remove trash

--- src/components/content/getData.js
+++ src/components/content/getData.js
@@ -1,52 +1,12 @@
 import {SERVER_URL} from "../../constants";
 import {toast} from "react-toastify";
 
-export const getCities = (setCities) => {
-    fetch(SERVER_URL + `/cities`)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            setCities(data)
-        })
-        .catch((e) => {
-            toast.error("🦄 Сервер решил полежать)")
-        })
-}
-
-export const getCustomers = (setCustomers) => {
-    fetch(SERVER_URL + `/customers`)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            setCustomers(data)
-        })
-        .catch((e) => {
-            toast.error("🦄 Сервер решил полежать)")
-        })
-}
-
-export const getMasters = (setMasters) => {
-    fetch(SERVER_URL + `/masters`)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            setMasters(data)
-        })
-        .catch((e) => {
-            toast.error("🦄 Сервер решил полежать)")
-        })
-}
-
 export const getDepsCityIdMasters = (setDeps, city_id) => {
-    fetch(SERVER_URL + `/deps/` + city_id)
+    fetch(SERVER_URL + `/deps/city/` + city_id)
         .then((res) => {
             return res.json()
         })
         .then((data) => {
-            data = data.map(d => d.master_id)
             setDeps(data)
         })
         .catch((e) => {
@@ -60,24 +20,9 @@ export const getDepsMasterIdCities = (setDeps, master_id) => {
             return res.json()
         })
         .then((data) => {
-            data = data.map(d => d.city_id)
             setDeps(data)
         })
         .catch((e) => {
             toast.error("🦄 Сервер решил полежать)")
         })
 }
-
-export const getOrders = (setOrders) => {
-    fetch(SERVER_URL + `/orders`)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            setOrders(data)
-        })
-        .catch((e) => {
-            toast.error("🦄 Сервер решил полежать)")
-        })
-}
-
