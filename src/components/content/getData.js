4a7a0b2aa3aa2722d1d1f832f4260f9193e7d155
loksblnine <illia.zinchenko@clockwise.software>
2021-11-16 17:57:58 +0200

    begin: refactor code duplicates

--- src/components/content/getData.js
+++ src/components/content/getData.js
@@ -1,8 +1,13 @@
 import {SERVER_URL} from "../../constants";
 import {toast} from "react-toastify";
+import axios from "axios";
 
 export const getDepsCityIdMasters = (setDeps, city_id) => {
-    fetch(SERVER_URL + `/deps/city/` + city_id)
+    fetch(SERVER_URL + `/deps/city/` + city_id, {
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem('token')}`
+        }
+    })
         .then((res) => {
             return res.json()
         })
@@ -15,7 +20,11 @@ export const getDepsCityIdMasters = (setDeps, city_id) => {
 }
 
 export const getDepsMasterIdCities = (setDeps, master_id) => {
-    fetch(SERVER_URL + `/deps/master/` + master_id)
+    fetch(SERVER_URL + `/deps/master/` + master_id, {
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem('token')}`
+        }
+    })
         .then((res) => {
             return res.json()
         })
@@ -25,4 +34,12 @@ export const getDepsMasterIdCities = (setDeps, master_id) => {
         .catch((e) => {
             toast.error("🦄 Сервер решил полежать)")
         })
+}
+export const getCitiesIntoStore = async (DB) => {
+    axios.get(SERVER_URL + `/cities`, {
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem('token')}`
+        }
+    })
+        .then(resp => DB.setCities(resp.data))
 }
