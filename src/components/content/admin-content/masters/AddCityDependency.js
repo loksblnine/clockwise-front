246890b1158e38f99e09f478cdd7c619ce3da469
loksblnine <illia.zinchenko@clockwise.software>
2021-11-17 13:57:03 +0200

    remove trash

--- src/components/content/admin-content/masters/AddCityDependency.js
+++ src/components/content/admin-content/masters/AddCityDependency.js
@@ -1,20 +1,19 @@
 import React, {Fragment, useContext, useState} from "react";
 import {SERVER_URL} from "../../../../constants";
 import {toast} from "react-toastify";
-import axios from "axios";
 import {Context} from "../../../../index";
+import {getAllDepsIntoStore} from "../../getData";
+import {observer} from "mobx-react-lite";
 
-const AddCityDependency = ({master}) => {
+const AddCityDependency = observer(({master}) => {
     const inputRef = React.useRef(null)
-    const [masterId, setMasterId] = useState(master.master_id)
-    const [masterName, setMasterName] = useState(master.master_name)
-    const [cityId, setCityId] = useState("")
+    const [cityId, setCityId] = useState("-1")
     const {DB} = useContext(Context);
 
     const onSubmitForm = async e => {
         e.preventDefault();
         try {
-            const body = {city_id: cityId, master_id: masterId}
+            const body = {city_id: cityId, master_id: master.master_id}
             await fetch(SERVER_URL + `/deps`, {
                 method: "POST",
                 headers: {
@@ -27,7 +26,6 @@ const AddCityDependency = ({master}) => {
             axios.get(SERVER_URL + `/deps`)
                 .then(resp => DB.setDepsMasterCity(resp.data))
             inputRef.current.click()
-
         } catch (e) {
             toast.info("Server is busy at this moment")
         }
@@ -35,27 +33,28 @@ const AddCityDependency = ({master}) => {
     return (
         <Fragment>
             <button type="button" className="btn btn-outline-success mb-5" data-toggle="modal"
-                    data-target={`#id_see${masterId}`}>
+                    data-target={`#id_see${master.master_id}`}>
                 Добавить
             </button>
-            <div className="modal fade" id={`id_see${masterId}`} tabIndex="-1" role="dialog"
+            <div className="modal fade" id={`id_see${master.master_id}`} tabIndex="-1" role="dialog"
                  aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <form onSubmit={event => onSubmitForm(event)}>
                             <div className="modal-header">
                                 <h2 className="modal-title" id="exampleModalLabel">Добавить город мастеру<br></br>
-                                    {masterName}</h2>
+                                    {master.master_name}</h2>
                                 <button type="button" className="close" data-dismiss="modal">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
                             <div className="modal-body">
                                 <label>Город</label>
-                                <select className="form-control" name={`city_id`} defaultValue={`-1`}
+                                <select className="form-control" name="city_id" defaultValue="-1"
+                                        onBlur={e=>setCityId(e.target.value)}
                                         onChange={e => setCityId(e.target.value)} required>
-                                    <option value={`-1`} disabled={true}>---Выбрать город---</option>
-                                    {DB.cities?.map(city =>
+                                    <option key="default" value="-1" disabled={true}>---Выбрать город---</option>
+                                    {DB.cities?.filter(c => !DB.depsMasterCity.find(d => d.master_id === master.master_id && c.city_id === d.city_id)).map(city =>
                                         <option key={city.city_id} value={city.city_id}>{city.city_name} </option>)}
                                 </select>
                             </div>
@@ -73,5 +72,5 @@ const AddCityDependency = ({master}) => {
             </div>
         </Fragment>
     )
-}
+})
 export default AddCityDependency;
