6ee96b2f2df955e4d12300024318b6a3c2ae2338
loksblnine <illia.zinchenko@clockwise.software>
2021-11-18 11:43:53 +0200

    using feat: instance of axios class

--- src/components/content/getData.js
+++ src/components/content/getData.js
@@ -1,64 +1,78 @@
-import {SERVER_URL} from "../../constants";
 import {toast} from "react-toastify";
-import axios from "axios";
-
-axios.interceptors.request.use(
-    {
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem('token')}`
-        }
-    })
+import {instance} from "../../http/headerPlaceholder.instance";
 
 export const getDepsCityIdMasters = (setDeps, city_id) => {
-    fetch(SERVER_URL + `/deps/city/` + city_id)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            setDeps(data)
-        })
+    instance({
+        method: "get",
+        url: `/deps/city/${city_id}`
+    })
+        .then(resp => setDeps(resp.data))
         .catch((e) => {
             toast.error("🦄 Сервер решил полежать)")
         })
 }
 
 export const getDepsMasterIdCities = (setDeps, master_id) => {
-    fetch(SERVER_URL + `/deps/master/` + master_id)
-        .then((res) => {
-            return res.json()
-        })
-        .then((data) => {
-            setDeps(data)
-        })
+    instance({
+        method: "get",
+        url: `/deps/master/${master_id}`
+    })
+        .then(resp => setDeps(resp.data))
         .catch((e) => {
             toast.error("🦄 Сервер решил полежать)")
         })
 }
 export const getCitiesIntoStore = async (DB) => {
-    axios.get(SERVER_URL + `/cities`)
+    instance({
+        method: "get",
+        url: "/cities"
+    })
         .then(resp => DB.setCities(resp.data))
 }
 export const getMastersIntoStore = async (DB) => {
-    axios.get(SERVER_URL + `/masters`)
+    instance({
+        method: "get",
+        url: "/masters"
+    })
         .then(resp => DB.setMasters(resp.data))
 }
 export const getCustomersIntoStore = async (DB) => {
-    axios.get(SERVER_URL + `/customers`)
+    instance({
+        method: "get",
+        url: "/customers"
+    })
         .then(resp => DB.setCustomers(resp.data))
 }
 export const getAllDepsIntoStore = async (DB) => {
-    axios.get(SERVER_URL + `/deps`)
+    instance({
+        method: "get",
+        url: "/deps"
+    })
         .then(resp => DB.setDepsMasterCity(resp.data))
 }
 export const getOrdersIntoStore = async (DB) => {
     sessionStorage.setItem('pageOrderList', "0")
-    axios.get(SERVER_URL + `/orders/offset/` + sessionStorage.getItem('pageOrderList'))
+    instance({
+        method: "get",
+        url: `/orders/offset/${sessionStorage.getItem('pageOrderList')}`
+    })
         .then(resp => DB?.setOrders(resp.data))
-        .then(()=>
-            axios.get(SERVER_URL + "/orders/offset/1")
+        .then(() =>
+            instance({
+                method: "get",
+                url: `/orders/offset/1`
+            })
                 .then(resp => DB?.setOrdersNext(resp.data))
                 .then(() => sessionStorage.setItem('pageOrderList',
                     (Number(sessionStorage.getItem('pageOrderList')) + 1).toString()))
         )
+
+    // axios.get(SERVER_URL + `/orders/offset/` + sessionStorage.getItem('pageOrderList'))
+    //     .then(resp => DB?.setOrders(resp.data))
+    //     .then(() =>
+    //         axios.get(SERVER_URL + "/orders/offset/1")
+    //             .then(resp => DB?.setOrdersNext(resp.data))
+    //             .then(() => sessionStorage.setItem('pageOrderList',
+    //                 (Number(sessionStorage.getItem('pageOrderList')) + 1).toString()))
+    //     )
 }
