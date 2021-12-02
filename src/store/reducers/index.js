f4e0f0eced18874af90ef251e141f4f91803d826
loksblnine <illia.zinchenko@clockwise.software>
2021-12-02 10:19:42 +0200

    add reducers

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,9 +3,13 @@ import {combineReducers} from 'redux';
 import cities from './cityReducer'
 import customers from './customerReducer'
 import orders from './orderReducer'
+import users from './userReducer'
+import masters from './masterReducer'
 
 export default combineReducers({
     cities,
     customers,
-    orders
+    orders,
+    users,
+    masters
 });
