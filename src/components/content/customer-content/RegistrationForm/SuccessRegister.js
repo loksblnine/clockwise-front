f844ce3b8c9fe44eff3f6b1f67219589ba781230
loksblnine <illya01@ukr.net>
2021-12-06 14:05:28 +0200

    add: registration form

---
+++ src/components/content/customer-content/RegistrationForm/SuccessRegister.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuccessRegister = () => {
+    return (
+        <div>
+            <h1>Проверьте свой почтовый адрес</h1>
+        </div>
+    );
+};
+
+export default SuccessRegister;
