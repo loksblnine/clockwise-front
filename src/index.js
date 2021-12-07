d7ee31194081390766f89966baf831ee9717a907
loksblnine <illia.zinchenko@clockwise.software>
2021-12-07 12:19:03 +0200

    remove trash

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React, {createContext} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from './components/App';
 import createStore from './store/store';
 import {Provider} from "react-redux";
 
 const store = createStore();
-export const Context = createContext(null)
 
 ReactDOM.render(
     <Provider store={store}>
