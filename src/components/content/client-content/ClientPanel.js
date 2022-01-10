015579d6436bda3e2e8ccf65af1925c59827e578
loksblnine <illya01@ukr.net>
2022-01-10 09:26:01 +0200

    code improve: svg -> constants.js + add components

--- src/components/content/client-content/ClientPanel.js
+++ src/components/content/client-content/ClientPanel.js
@@ -1,12 +1,20 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import * as constants from "../../../utils/constants";
-import {ARROWS_SVG} from "../../../utils/constants";
+import {ARROWS_SVG, STAR} from "../../../utils/constants";
 import {Spinner} from "react-bootstrap";
 import {useDispatch, useSelector} from "react-redux";
 import {setOrdersCustomer, sortOrders} from "../../../store/actions/orderActions";
 import {setUserData} from "../../../store/actions/userActions";
 import SetMarkDialog from "./SetMarkDialog";
 import EditProfileClient from "./EditProfileClient";
+
+const OrderMark = ({mark}) => {
+    return (
+        <div>
+            {mark} &nbsp; {STAR}
+        </div>
+    )
+}
 
 const ClientPanel = () => {
     const dispatch = useDispatch()
@@ -82,19 +90,15 @@ const ClientPanel = () => {
                                 <td>{order?.order_time?.split('T')[0]}</td>
                                 <td>{order?.order_time?.split('T')[1].split('.')[0]}</td>
                                 <td>{Number(order?.order_time?.split('T')[1]?.split(':')[0]) + Number(order?.work_id) + ":00:00"}</td>
-                                <td>{order?.isDone ? order?.mark ? (
-                                        <div>
-                                            {order.mark} &nbsp;
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                 fill="currentColor"
-                                                 className="bi bi-star" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
-                                            </svg>
-                                        </div>
-                                    ) :
-                                    <SetMarkDialog order={order}/>
-                                    : "Не готов"}</td>
+                                <td>
+                                    {
+                                        order?.isDone
+                                            ? order?.mark
+                                            ? <OrderMark mark={order.mark}/>
+                                            : <SetMarkDialog order={order}/>
+                                            : "Не готов"
+                                    }
+                                </td>
                             </tr>
                         ))}
                         </tbody>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -227,6 +227,26 @@ export const ARROWS_SVG = {
     </svg>
 }
 
+export const COLLAPSE_ARROWS =
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+         className="bi bi-arrows-collapse" viewBox="0 0 16 16">
+        <path fillRule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8zm7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0zm-.5 11.707-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0v-3.793z"/>
+    </svg>
+export const EXPAND_ARROWS =
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+         className="bi bi-arrows-expand" viewBox="0 0 16 16">
+        <path fillRule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8zM7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10z"/>
+    </svg>
+export const STAR =
+    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+         height="16"
+         fill="currentColor"
+         className="bi bi-star" viewBox="0 0 16 16">
+        <path
+            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+    </svg>
 export const SERVER_URL = process.env.NODE_ENV === "production" ? "https://enigmatic-spire-58695.herokuapp.com" : "http://localhost:5000"
 
 export const datePattern = "[0-9]{4}.[0-9]{2}.[0-9]{2}"
